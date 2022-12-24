185ee14e08c04d8454f8ead24d97a049ea9e1a16
Mohamed Elkammar <mohamed.elkammar@gmail.com>
2022-12-23 19:53:20 -0800

    Answering with a `PONG`.

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 const net = require("net");
 // const resp = require("resp");
+// const Parser = require("redis-parser");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -7,10 +8,19 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
-  console.log("Hello from my CodeCrafters' redis!");
+  console.log("Connection received from client.");
 
-  // console.log("connection", connection);
-  connection.on("connection", (stream) => console.log(stream));
+  connection.on("data", (data) => {
+    // console.log('resp.stringify("PONG")', resp.stringify("PONG"));
+    // connection.write(resp.stringify("PONG"));
+    connection.write("+PONG\r\n");
+  });
+
+  connection.on("end", () => {
+    console.log("client disconnected");
+  });
+  // connection.write("Hello, welcome to my Redis server.\r\n");
+  // connection.pipe(connection);
 });
 
 server.listen(6379, "127.0.0.1");

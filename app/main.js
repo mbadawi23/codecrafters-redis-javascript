383e8237547fc5a5fc6ecb7de85a30c792d68476
Mohamed Elkammar <mohamed.elkammar@gmail.com>
2022-12-23 18:09:49 -0800

    Added a `consol.log()`

--- app/main.js
+++ app/main.js
@@ -1,11 +1,15 @@
 const net = require("net");
+const net = require("resp");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-       // Handle connection
- });
+  // Handle connection
+  console.log("Hello from my CodeCrafters' redis!");
 
- server.listen(6379, "127.0.0.1");
+  console.log("connection", connection);
+});
+
+server.listen(6379, "127.0.0.1");

bb3407a316a712f9390f8b252621bca91c116c73
Mohamed Elkammar <mohamed.elkammar@gmail.com>
2022-12-25 15:23:48 -0800

    Replaced old parsing with new `Resp` parser.

--- app/main.js
+++ app/main.js
@@ -29,6 +29,10 @@ class Resp {
     }
   }
 
+  encodeError(error) {
+    return `-${error}\r\n`;
+  }
+
   parse(data) {
     if (data.length === 0) return undefined;
 
@@ -80,38 +84,24 @@ const server = net.createServer((connection) => {
   connection.on("data", (buffer) => {
     const data = buffer.toString();
     console.log("data", data);
-    console.log("parser.parse(data)", parser.parse(data));
 
-    if (data === "+PING") {
-      connection.write("+PONG\r\n");
-    } else if (data[0] === "*") {
-      console.log("Parsing array...");
-      const parseArray = (resp) => {
-        console.log("resp", resp);
-        const rawArr = resp.split(/\r?\n/);
-        console.log("rawArr", rawArr);
-        const filtered = rawArr.filter(
-          (item) => !/[\*\$\+\-]/.test(item) && !/^(?![\s\S])/.test(item)
-        );
-        console.log("filtered", filtered);
-        return filtered;
-      };
-      const respArr = parseArray(data);
-      respArr.forEach((item, i) => {
-        if (item.toUpperCase() === "PING") {
-          console.log("PONG");
-          connection.write("+PONG\r\n");
-        } else if (item.toUpperCase() === "ECHO") {
-          console.log("ECHO", respArr[i + 1]);
-          if (i + 1 < respArr.length)
-            connection.write(`+${respArr[i + 1]}\r\n`);
-          else connection.write("ERR wrong number of arguments for command");
-        } else {
-          console.log("Command Not Supported!!!");
-          connection.write(`-ERR unknown command ${item}\r\n`);
-        }
-      });
-    }
+    const parsed = parser.parse(data);
+    console.log("parsed", parsed);
+    parsed.forEach((item, i) => {
+      if (item.toUpperCase() === "PING") {
+        console.log("PONG");
+        connection.write(parser.PONG);
+      } else if (item.toUpperCase() === "ECHO") {
+        if (i + 1 < parsed.length) {
+          connection.write(`+${parsed[i + 1]}\r\n`);
+        } else
+          connection.write(
+            parser.encodeError("ERR wrong number of arguments for command")
+          );
+      } else {
+        connection.write(`-ERR unknown command ${item}\r\n`);
+      }
+    });
   });
 
   connection.on("end", () => {

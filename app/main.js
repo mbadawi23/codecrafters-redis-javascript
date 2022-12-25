8d3b47df6fc49d4e812e521c667f6ef48cec139f
Mohamed Elkammar <mohamed.elkammar@gmail.com>
2022-12-25 15:11:09 -0800

    Couldn't get `require` to work with `resp-parser`...
    Copied the whole thing in `main.js`.

--- app/main.js
+++ app/main.js
@@ -1,5 +1,71 @@
 const net = require("net");
-var Resp = require("./resp-parser");
+
+class Resp {
+  constructor() {
+    this.EMPTY_STRING = "$0\r\n\r\n";
+    this.NULL_STRING = "$-1\r\n";
+    this.NULL_ARRAY = "*-1\r\n";
+    this.OK = "+OK\r\n";
+    this.PONG = "+PONG\r\n";
+  }
+
+  encode(data) {
+    if (data instanceof Array) {
+      let encoded = `*${data.length}\r\n`;
+      data.forEach((item) => (encoded = encoded.concat(this.encode(data))));
+      return encoded;
+    } else {
+      switch (typeof data) {
+        case "string":
+          return `\$${data.length}\r\n${data}\r\n`;
+        case "number":
+          return `:${data.toString()}\r\n`;
+        case "boolean":
+          return `\$${data.toString().length}\r\n${data.toString()}\r\n`;
+        case "bigint":
+        default:
+          return this.NULL_STRING;
+      }
+    }
+  }
+
+  parse(data) {
+    if (data.length === 0) return undefined;
+
+    switch (data[0]) {
+      case "*":
+        return this.#parseArray(data);
+      case "$":
+        return this.#parseString(data);
+      case ":":
+        return this.#parseNumber(data);
+      default:
+        break;
+    }
+  }
+
+  #parseArray(data) {
+    const rawArr = data.split(/\r?\n/);
+    const filtered = rawArr.filter(
+      (item) => !/[\*\$\+\-]/.test(item) && !/^(?![\s\S])/.test(item)
+    );
+    return filtered;
+  }
+
+  #parseString(data) {
+    return data
+      ? data.substring(data.indexOf("\r\n") + 2).replace("\r\n", "")
+      : "";
+  }
+
+  #parseNumber(data) {
+    return Number(data.slice(1).replace("\r\n", ""));
+  }
+
+  // TODO: to avoid recursion in encode()
+  // #encodeSimpleTypes(data) {  }
+}
+
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
@@ -9,6 +75,7 @@ const server = net.createServer((connection) => {
   console.log("Connection received from client.");
 
   const parser = new Resp();
+  console.log("parser.OK", parser.OK);
 
   connection.on("data", (buffer) => {
     const data = buffer.toString();

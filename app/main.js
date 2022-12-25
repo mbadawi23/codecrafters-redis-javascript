326fe6531f29dbef2194ef0f5d0b1e82e8c7af09
Mohamed Elkammar <mohamed.elkammar@gmail.com>
2022-12-24 18:40:20 -0800

    Checking Stage #5 response

--- app/main.js
+++ app/main.js
@@ -1,6 +1,4 @@
 const net = require("net");
-// const resp = require("resp");
-// const Parser = require("redis-parser");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -10,10 +8,28 @@ const server = net.createServer((connection) => {
   // Handle connection
   console.log("Connection received from client.");
 
-  connection.on("data", (data) => {
-    // console.log('resp.stringify("PONG")', resp.stringify("PONG"));
-    // connection.write(resp.stringify("PONG"));
-    connection.write("+PONG\r\n");
+  connection.on("data", (buffer) => {
+    const data = buffer.toString();
+    console.log("data", data);
+
+    if (data === "+PING") {
+      connection.write("+PONG\r\n");
+    } else if (data[0] === "*") {
+      console.log("Parsing array...");
+      const parseArray = (resp) => {
+        console.log("resp", resp);
+        const rawArr = resp.split(/\r?\n/);
+        console.log("rawArr", rawArr);
+        const arr = rawArr.reduce((result, item) => {
+          console.log("item", item);
+          const re = /[\*\$\+\-]*/;
+          if (re.test(item)) return result;
+        }, []);
+        console.log("arr", arr);
+        return resp;
+      };
+      parseArray(data);
+    }
   });
 
   connection.on("end", () => {
